2e33ba9778485d1b6ec48f8f212fe286a5e1c1ce
eneserciyes <m.eneserciyes@gmail.com>
2024-04-19 16:24:12 -0400

    depth flag set

--- data_collect.py
+++ data_collect.py
@@ -3,7 +3,7 @@ from openteach.components import Collector
 
 @hydra.main(version_base = '1.2', config_path = 'configs', config_name = 'collect_data')
 def main(configs):
-    collector = Collector(configs, configs.demo_num)
+    collector = Collector(configs, configs.demo_num, depth=True)
     processes = collector.get_processes()
 
     for process in processes:
