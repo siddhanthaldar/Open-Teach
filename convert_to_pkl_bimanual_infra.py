Fixed rotation issues

convert_to_pkl_bimanual_infra.py
@@ -7,10 +7,10 @@ from pathlib import Path
 from scipy.spatial.transform import Rotation
 from sentence_transformers import SentenceTransformer
 
-FOLDER_NAME = "2024.05.01"
+FOLDER_NAME = "2024.05.05"
 PROCESSED_DATA_PATH = Path(f"/mnt/robotlab/siddhant/projects/scaling_polytask/processed_data/{FOLDER_NAME}")
-SAVE_DATA_PATH = Path(f"/mnt/robotlab/siddhant/projects/scaling_polytask/processed_data_pkl/")
-task_names = ["fridge_pick_yoghurt"]
+SAVE_DATA_PATH = Path(f"/mnt/robotlab/siddhant/projects/scaling_polytask/processed_data_pkl_aa/")
+task_names = None #["fridge_pick_yoghurt"]
 camera_indices = [1,2,3,4,51,52]
 img_size = (128, 128)
 NUM_DEMOS = None
@@ -106,12 +106,12 @@ for TASK_NAME in task_names:
         #             while angles[idx][i] < 0:
         #                 angles[idx][i] += 2*np.pi
         #     return angles
-        cartesian_pos = cartesian_states[:, :3]
-        cartesian_ori = cartesian_states[:, 3:]
-        cartesian_ori = Rotation.from_rotvec(cartesian_ori).as_euler('xyz')
-        # cartesian_ori = wrap_angle(cartesian_ori)
-        cartesian_ori = np.concatenate([np.sin(cartesian_ori), np.cos(cartesian_ori)], axis=1)
-        cartesian_states = np.concatenate([cartesian_pos, cartesian_ori], axis=1)
+        # cartesian_pos = cartesian_states[:, :3]
+        # cartesian_ori = cartesian_states[:, 3:]
+        # cartesian_ori = Rotation.from_rotvec(cartesian_ori).as_euler('xyz')
+        # # cartesian_ori = wrap_angle(cartesian_ori)
+        # cartesian_ori = np.concatenate([np.sin(cartesian_ori), np.cos(cartesian_ori)], axis=1)
+        # cartesian_states = np.concatenate([cartesian_pos, cartesian_ori], axis=1)
         # rest
         gripper_states = state['gripper_state'].values.astype(np.float32)
         observation["cartesian_states"] = cartesian_states.astype(np.float32)

convert_to_pkl_bimanual_infra_aa.py
@@ -7,9 +7,9 @@ from pathlib import Path
 from scipy.spatial.transform import Rotation as R
 from sentence_transformers import SentenceTransformer
 
-FOLDER_NAME = "2024.04.23"
+FOLDER_NAME = "2024.05.05"
 PROCESSED_DATA_PATH = Path(f"/mnt/robotlab/siddhant/projects/scaling_polytask/processed_data/{FOLDER_NAME}")
-SAVE_DATA_PATH = Path(f"/mnt/robotlab/siddhant/projects/scaling_polytask/processed_data_pkl_aa/")
+SAVE_DATA_PATH = Path(f"/mnt/robotlab/siddhant/projects/scaling_polytask/processed_data_pkl_aa_new/")
 task_names = None #["lift_pan_lid", "place_pan_lid"]
 camera_indices = [1,2,3,4,51,52]
 img_size = (128, 128)

openteach/components/deploy/deployer_xarm.py
@@ -59,7 +59,7 @@ class DeployServer(Component):
                         gripper_action = robot_action_dict[robot]['gripper']
                         cartesian_coords = robot_action_dict[robot]['cartesian']
 
-                        self._robots[robot].set_desired_pose(cartesian_coords, 800 if gripper_action > 0.5 else 0)
+                        self._robots[robot].set_desired_pose(cartesian_coords, 800 if gripper_action > 0.7 else 0)
 
 
                     concat_action = np.concatenate([robot_action_dict[robot]['cartesian'], robot_action_dict[robot]['gripper']])       

openteach/ros_links/bimanual.py
@@ -98,8 +98,29 @@ class DexArmControl():
     def get_arm_torque(self):
         raise ValueError('get_arm_torque() is being called - Arm Torques cannot be collected in Franka arms, this method should not be called')
 
+    # def make_orientation_sign_consistent(self, axis):
+    #     reference_axis = np.array([1, 1, 1])
+    #     if np.dot(axis, reference_axis) < 0:
+    #         axis = -axis
+    #     return axis
+
+    def make_orientation_sign_consistent(self, axis):
+        norm_last = np.linalg.norm(self.prev_ori)
+        norm_curr = np.linalg.norm(axis)
+        dot_product = np.dot(axis, self.prev_ori)
+        if abs(norm_last - norm_curr) < 0.2 and dot_product < 0:
+            axis = -axis
+        return axis
+
+
     def get_arm_cartesian_coords(self):
         status, home_pose = self.robot.get_position_aa()
+        home_pose = np.array(home_pose)
+        # if not hasattr(self, 'prev_ori'):
+        #     self.prev_ori = home_pose[3:6]
+        # else:
+        #     home_pose[3:6] = self.make_orientation_sign_consistent(home_pose[3:6])
+        #     self.prev_ori = home_pose[3:6]
         return home_pose
 
     def get_gripper_state(self):
@@ -135,7 +156,31 @@ class DexArmControl():
         # ori = R.from_euler('xyz', ori).as_rotvec()      
         # # desired
         # desired_cartesian_pose = np.concatenate([pos, ori])
-        desired_cartesian_pose = curr_cartesian_pose + cartesian_pose
+        # find current matrix
+        pos_curr = curr_cartesian_pose[:3]
+        ori_curr = curr_cartesian_pose[3:]
+        r_curr = R.from_rotvec(ori_curr).as_matrix()
+        matrix_curr = np.eye(4)
+        matrix_curr[:3, :3] = r_curr
+        matrix_curr[:3, 3] = pos_curr
+        # find transformation matrix
+        pos_delta = cartesian_pose[:3]
+        ori_delta = cartesian_pose[3:]
+        r_delta = R.from_rotvec(ori_delta).as_matrix()
+        matrix_delta = np.eye(4)
+        matrix_delta[:3, :3] = r_delta
+        matrix_delta[:3, 3] = pos_delta
+        # find desired matrix
+        matrix_desired = matrix_curr @ matrix_delta
+        # matrix_desired = matrix_delta @ matrix_curr
+        # pos_desired = matrix_desired[:3, 3]
+        pos_desired = pos_curr + pos_delta
+        r_desired = matrix_desired[:3, :3]
+        ori_desired = R.from_matrix(r_desired).as_rotvec()
+        desired_cartesian_pose = np.concatenate([pos_desired, ori_desired])
+        # print("current_cartesian_pose", curr_cartesian_pose)
+        # print("desired_cartesian_pose", desired_cartesian_pose)
+        # desired_cartesian_pose = curr_cartesian_pose + cartesian_pose
 
         # desired gripper pose
         self.apply_gripper=False
@@ -189,10 +234,11 @@ class DexArmControl():
         return joint_state
         
     def get_cartesian_state(self):
-        status,current_pos=self.robot.get_position_aa()
+        # status,current_pos=self.robot.get_position_aa()
+        current_pos = self.get_arm_cartesian_coords()
 
         pos, ori = current_pos[:3], current_pos[3:]
-        ori = R.from_rotvec(ori).as_euler('xyz')
+        # ori = R.from_rotvec(ori).as_euler('xyz')
         cartesian_state = dict(
             position = np.array(pos, dtype=np.float32).flatten(),
             orientation = np.array(ori, dtype=np.float32).flatten(),